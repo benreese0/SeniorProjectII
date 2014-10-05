7bd7f2f93a69f1cb5a46fbcc0cd50a93d77e9ca8
jyl11002 <james1002@gmail.com>
2014-10-05 10:47:52 -0700

    Arduino Code Initial Commit
    Just getting the file on here

---
+++ car_arduino/car_arduino.ino
@@ -0,0 +1,15 @@
+// Self Driving Car - Arduino Code
+// Theresa Cruz, Jessica Dudoff, James Lee, and Ben Reese
+// ECPE 196 - Fall 2014
+
+// Have not yet definied pins/variables
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
+
+

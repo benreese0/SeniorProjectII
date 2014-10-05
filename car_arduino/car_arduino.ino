4fa208e30301d98a1ad720adcac0162a918e595b
James Lee <james1002@gmail.com>
2014-10-05 11:25:51 -0700

    Working on a basic structure WITH comments :D

--- car_arduino/car_arduino.ino
+++ car_arduino/car_arduino.ino
@@ -9,7 +9,37 @@ void setup() {
 }
 
 void loop() {
-  
+// For now, assuming that I have taken in the UART message and 
+// decoded it into a char for the command, and an int or double for
+// the parameter
+
+// Assumptions - name of the char will be comm
+// name of the parameter will be inX
+    switch (comm) {
+    case 'S':
+        
+    case 'F':
+        // Set speed to parameter
+
+    case 'R':
+        // Set direction servo and camera servo to 
+        //parameter in the right direction
+
+    case 'L':
+        // Set direction servo and camera servo to 
+        //parameter in the left direction
+
+    case 'B':
+        // Stop Car, then set reverse speed to given parameter
+
+    case 'A':
+        // 
+    case 'E':
+
+
+
+
+
 }
 
 

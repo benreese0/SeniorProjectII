f1d793ad711c00fd04551eecf98a9735e07b50cc
Ben Reese <ben.reese.0@gmail.com>
2014-11-24 12:23:02 -0800

    added net img stream stuff

---
+++ pi_comm/origNetStreamServConnect.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import io
+import socket
+import struct
+from PIL import Image
+
+# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
+# all interfaces)
+client_socket = socket.socket()
+client_socket.connect(('192.168.1.4', 8000))
+
+# Accept a single connection and make a file-like object out of it
+connection = client_socket.accept()[0].makefile('w+b')
+try:
+    while True:
+        # Read the length of the image as a 32-bit unsigned int. If the
+        # length is zero, quit the loop
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))
+        print ("len:" + str(image_len))
+        image_len = image_len[0]
+        print ("len[0]:" + str(image_len))
+
+
+        if not image_len:
+            break
+        # Construct a stream to hold the image data and read the image
+        # data from the connection
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+        # Rewind the stream, open it as an image with PIL and do some
+        # processing on it
+        image_stream.seek(0)
+        image = Image.open(image_stream)
+        print('Image is %dx%d' % image.size)
+        image.verify()
+        print('Image is verified')
+finally:
+    connection.close()
+    server_socket.close()

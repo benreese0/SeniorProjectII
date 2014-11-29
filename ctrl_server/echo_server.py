abb4fe71819817744edb24c2734a12a189f6ba10
Ben Reese <ben.reese.0@gmail.com>
2014-11-29 13:33:47 -0800

    Working on getting lines calibrated

--- ctrl_server/echo_server.py
+++ ctrl_server/echo_server.py
@@ -1,29 +1,39 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import socket
 import select
+import struct
+
 
 #constants
 cmd_port = 7777
-img_port = 7780
+img_port = 7778
+lines_port = 7780
 pi_addr = '192.168.1.7'
 img_addr = '192.168.1.6'
 ctrl_addr = '192.168.1.6'
 fourmb = 1024*1024*4
 
+
+currangle = 8
+
 #static sockets
 pi_img = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-pi_img.connect( (pi_addr, 7778) )
+pi_img.connect( (pi_addr, img_port) )
 print("Pi img connection made")
 pi_cmd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-pi_cmd.connect( (pi_addr, 7777) )
+pi_cmd.connect( (pi_addr, cmd_port) )
 print("Pi cmd connection made")
 ctrl_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ctrl_sock.connect( (ctrl_addr, 7777) )
+ctrl_sock.connect( (ctrl_addr, cmd_port) )
 print("Ctrl connection made")
 img_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-img_sock.connect( (img_addr, 7779) )
+img_sock.connect( (img_addr, lines_port) )
 print("Img connection made")
+
+
+#startup with centered wheels
+#pi_cmd.sendall('L8\n')
 
 sources = [pi_img, pi_cmd, img_sock, ctrl_sock]
 
@@ -33,17 +43,24 @@ while True:
   for src in inrdy:
    if src == pi_img: #new image from pi
     data = pi_img.recv(fourmb)
-    #while data.length() >0: 
-     #   print("lenn" + str(data.len()))
     img_sock.sendall(data)
-      #  data = pi_img.recv(fourmb)
    elif src == pi_cmd:#data from pi
     data = pi_cmd.recv(1024)
     print("Arduino:" + str(data))
    elif src == img_sock:#cmd from img_proc
     data = img_sock.recv(1024)
-    print("IMGCMD:"+ str(data))
-    #pi_cmd.sendall(data)
+    data = str(data).strip('\x00')
+    newangle = int(float(data))/4
+    print("newangle"+ str(newangle))
+    currangle = newangle + currangle
+    print("next currangle:"+ str(currangle))
+    if newangle > 1 or newangle < -1:
+     if currangle >0:
+      cmd = 'R' + str(currangle) + '\n'
+     else:
+      cmd = 'L' + str(-1*currangle) +'\n'
+     print("command" + repr(cmd))
+     pi_cmd.sendall(cmd)
    elif src == ctrl_sock:#cmd from ctrl
     data = ctrl_sock.recv(1024)
     pi_cmd.sendall(data)
@@ -51,8 +68,6 @@ while True:
     print("got unexpected data:")
     print(src.recv(1024))
  except KeyboardInterrupt:
-  break
- except ConnectionRefusedError:
   break
 print('Exiting')
 
